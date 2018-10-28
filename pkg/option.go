fcb83891ecefe2556e060d98679929f0105e98ab
Louis Thibault <l.thibault@sentimens.com>
2018-10-28 20:25:00 +0000

    Switch filenames between casm.go and option.go

--- pkg/option.go
+++ pkg/option.go
@@ -1,38 +1,78 @@
 package casm
 
 import (
-	"context"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/config"
 
-	libp2p "github.com/libp2p/go-libp2p"
-	host "github.com/libp2p/go-libp2p-host"
-	"github.com/pkg/errors"
+	ma "github.com/multiformats/go-multiaddr"
 )
 
-// Host is a logical machine in a compute cluster.  It acts both as a server and
-// a client.  In the CASM expander-graph model, it is a vertex.
-type Host struct {
-	h host.Host
-	k uint8
+// Option represents a setting
+type Option interface {
+	Opt()
 }
 
-// New Host whose lifetime is bound to the context c.
-func New(c context.Context, opt ...Option) (h *Host, err error) {
-	copt := defaultHostOpts()
-	copt.Load(opt)
+/***************************************
+Adapters for libp2p Host options
+****************************************/
 
-	popt := defaultP2pOpts()
-	popt.Load(opt)
+// p2pOpt allows for the annotation of libp2p Options so that they can be
+// recognized by New() and passed to libp2p.New().
+type p2pOpt func(*config.Config) error
 
-	h = new(Host)
-	if h.h, err = libp2p.New(c, popt...); err != nil {
-		err = errors.Wrap(err, "libp2p")
-	}
+func (p2pOpt) Opt() {}
 
-	for _, o := range copt {
-		if err = o.Apply(h); err != nil {
-			break
+type p2pOptions []libp2p.Option
+
+func defaultP2pOpts() p2pOptions {
+	panic("defaultLibp2pOpts NOT IMPLEMENTED")
+}
+
+func (h p2pOptions) Load(opt []Option) {
+	for _, o := range opt {
+		if op, ok := o.(p2pOpt); ok {
+			h = append(h, libp2p.Option(op))
 		}
 	}
+}
 
-	return
+// ListenAddrStrings configures the CASM host to listen on the given multiaddrs
+func ListenAddrStrings(s ...string) Option {
+	return p2pOpt(libp2p.ListenAddrStrings(s...))
+}
+
+// ListenAddrs configures the CASM host to listen on the given multiaddrs
+func ListenAddrs(addrs ...ma.Multiaddr) Option {
+	return p2pOpt(libp2p.ListenAddrs(addrs...))
+}
+
+/************************
+	CASM Host options
+*************************/
+
+type hostOpt func(*Host) error
+
+func (hostOpt) Opt()                    {}
+func (opt hostOpt) Apply(h *Host) error { return opt(h) }
+
+type hostOptions []hostOpt
+
+func defaultHostOpts() hostOptions {
+	panic("defaultHostOpts NOT IMPLEMENTED")
+}
+
+func (h hostOptions) Load(opt []Option) {
+	for _, o := range opt {
+		if op, ok := o.(hostOpt); ok {
+			h = append(h, op)
+		}
+	}
+}
+
+// Cardinality configures the number of
+func Cardinality(k uint8) Option {
+	return hostOpt(func(h *Host) error {
+		h.k = k
+		return nil
+	})
 }
