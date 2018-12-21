35158d96e12cfb58d4708bb66a6880f455bfe0e1
Louis Thibault <l.thibault@sentimens.com>
2018-12-21 12:52:36 +0100

    Move basicHost to its own file

---
+++ pkg/host/basic.go
@@ -0,0 +1,183 @@
+package host
+
+import (
+	"context"
+
+	"github.com/SentimensRG/ctx"
+	casm "github.com/lthibault/casm/pkg"
+	net "github.com/lthibault/casm/pkg/net"
+	log "github.com/lthibault/log/pkg"
+	"github.com/pkg/errors"
+)
+
+type basicHost struct {
+	log   log.Logger
+	c     context.Context
+	a     net.Addr
+	mux   *streamMux
+	peers *peerStore
+	t     net.Transport
+}
+
+func (bh basicHost) Addr() net.Addr { return bh.a }
+
+func (bh basicHost) Network() Network {
+	if bh.c == nil {
+		panic(errors.New("host not started"))
+	}
+	return bh
+}
+
+func (bh basicHost) Stream() StreamManager { return bh }
+
+func (bh basicHost) Context() context.Context {
+	if bh.c == nil {
+		panic(errors.New("host not started"))
+	}
+	return bh.c
+}
+
+func (bh *basicHost) Start(c context.Context) error {
+	bh.log = bh.log.WithFields(log.F{
+		"id":         bh.a.ID(),
+		"local_peer": bh.a,
+	})
+
+	bh.c = log.Set(c, bh.log)
+	c = log.Set(c, bh.log.WithLocus("transport"))
+
+	l, err := bh.t.Listen(c, bh.a)
+	if err != nil {
+		return errors.Wrap(err, "listen")
+	}
+
+	ctx.Defer(bh.c, func() { l.Close() })
+	go bh.startAccepting(l)
+
+	bh.log.Info("started host")
+	return nil
+}
+
+func (bh basicHost) startAccepting(l net.Listener) {
+	var err error
+	var conn *net.Conn
+
+	for range ctx.Tick(bh.c) {
+		if conn, err = l.Accept(); err != nil {
+			bh.log.WithError(err).Warn("failed to accept conn")
+			return
+		}
+
+		if !bh.peers.Add(conn) {
+			bh.log.Error("peer already connected")
+			conn.Close()
+			return
+		}
+
+		l := bh.log.WithField("remote_peer", conn.RemoteAddr())
+
+		l.Debug("connection accepted")
+		go bh.handle(conn.WithContext(log.Set(conn.Context(), l)))
+	}
+}
+
+func (bh basicHost) handle(conn *net.Conn) {
+	defer bh.Disconnect(conn.RemoteAddr())
+
+	var err error
+	var s *net.Stream
+	for range ctx.Tick(ctx.Link(bh.c, conn.Context())) {
+		if s, err = conn.AcceptStream(); err != nil {
+			bh.log.WithError(err).Warn("failed to accept stream")
+			return
+		}
+
+		l := bh.log.WithFields(log.F{
+			"remote_peer": s.RemoteAddr(),
+			"stream":      s.StreamID(),
+		})
+		l.Debug("handling stream")
+
+		c := log.Set(s.Context(), l)
+		go bh.handleStream(s.WithContext(c))
+	}
+}
+
+func (bh basicHost) handleStream(s *net.Stream) {
+	var p net.Path
+	if _, err := p.ReadFrom(s); err != nil {
+		bh.log.WithError(err).Error("failed to read stream path")
+		return
+	}
+
+	l := bh.log.WithFields(log.F{"locus": "handler", "path": p})
+	c := log.Set(s.Context(), l)
+
+	bh.mux.Serve(p.String(), s.WithContext(c))
+}
+
+/*
+	implment StreamManager
+*/
+
+func (bh basicHost) Register(path string, h net.Handler) {
+	c := log.Set(bh.c, bh.log.WithLocus("mux"))
+	bh.mux.Register(c, path, h)
+}
+
+func (bh basicHost) Unregister(path string) { bh.mux.Unregister(path) }
+
+func (bh basicHost) Open(a casm.Addresser, path string) (*net.Stream, error) {
+	conn, ok := bh.peers.Get(a.Addr())
+	if !ok {
+		return nil, errors.New("peer not connected")
+	}
+
+	s, err := conn.OpenStream()
+	if err != nil {
+		return nil, errors.Wrap(err, "open stream")
+	}
+
+	l := bh.log.WithField("stream", s.StreamID())
+	l.Debug("stream opened")
+
+	if _, err = net.Path(path).WriteTo(s); err != nil {
+		return nil, errors.Wrap(err, "write path")
+	}
+
+	l = l.WithField("path", path)
+	l.Debug("header sent")
+
+	c := log.Set(s.Context(), l)
+	return s.WithContext(c), nil
+}
+
+/*
+	Implement Network
+*/
+
+func (bh basicHost) Connect(c context.Context, a casm.Addresser) error {
+
+	if _, connected := bh.peers.Get(a.Addr()); connected {
+		return errors.Errorf("%s already connected", a.Addr().ID())
+	}
+
+	conn, err := bh.t.Dial(c, a.Addr())
+	if err != nil {
+		return errors.Wrap(err, "transport")
+	}
+
+	if !bh.peers.Add(conn) {
+		conn.Close()
+		return errors.New("peer already connected")
+	}
+
+	bh.log.WithField("remote_peer", conn.RemoteAddr()).Debug("connected to peer")
+	return nil
+}
+
+func (bh basicHost) Disconnect(id casm.IDer) {
+	if conn, ok := bh.peers.Del(id.ID()); ok {
+		conn.Close()
+	}
+}
--- pkg/host/host.go
+++ pkg/host/host.go
@@ -3,12 +3,8 @@ package host
 import (
 	"context"
 
-	"github.com/SentimensRG/ctx"
-
 	casm "github.com/lthibault/casm/pkg"
 	net "github.com/lthibault/casm/pkg/net"
-	log "github.com/lthibault/log/pkg"
-	"github.com/pkg/errors"
 )
 
 // Network manages raw connections
@@ -20,8 +16,8 @@ type Network interface {
 // StreamManager manages streams, which are multiplexed on top of raw connections
 type StreamManager interface {
 	Register(string, net.Handler)
-	Unregister(net.Path)
-	Open(casm.Addresser, net.Path) (*net.Stream, error)
+	Unregister(string)
+	Open(casm.Addresser, string) (*net.Stream, error)
 }
 
 // Host is a logical machine in a compute cluster.  It acts both as a server and
@@ -34,15 +30,6 @@ type Host interface {
 	Start(c context.Context) error
 }
 
-type basicHost struct {
-	log   log.Logger
-	c     context.Context
-	a     net.Addr
-	mux   *streamMux
-	peers *peerStore
-	t     net.Transport
-}
-
 // New Host.  Pass options to override defaults.
 func New(opt ...Option) Host {
 	c := cfg{}
@@ -51,167 +38,4 @@ func New(opt ...Option) Host {
 	}
 
 	return mkHost(c)
-}
-
-func (bh basicHost) Addr() net.Addr { return bh.a }
-
-func (bh basicHost) Network() Network {
-	if bh.c == nil {
-		panic(errors.New("host not started"))
-	}
-	return bh
-}
-
-func (bh basicHost) Stream() StreamManager { return bh }
-
-func (bh basicHost) Context() context.Context {
-	if bh.c == nil {
-		panic(errors.New("host not started"))
-	}
-	return bh.c
-}
-
-func (bh *basicHost) Start(c context.Context) error {
-	bh.log = bh.log.WithFields(log.F{
-		"id":         bh.a.ID(),
-		"local_peer": bh.a,
-	})
-
-	bh.c = log.Set(c, bh.log)
-	c = log.Set(c, bh.log.WithLocus("transport"))
-
-	l, err := bh.t.Listen(c, bh.a)
-	if err != nil {
-		return errors.Wrap(err, "listen")
-	}
-
-	ctx.Defer(bh.c, func() { l.Close() })
-	go bh.startAccepting(l)
-
-	bh.log.Info("started host")
-	return nil
-}
-
-func (bh basicHost) startAccepting(l net.Listener) {
-	var err error
-	var conn *net.Conn
-
-	for range ctx.Tick(bh.c) {
-		if conn, err = l.Accept(); err != nil {
-			bh.log.WithError(err).Warn("failed to accept conn")
-			return
-		}
-
-		if !bh.peers.Add(conn) {
-			bh.log.Error("peer already connected")
-			conn.Close()
-			return
-		}
-
-		l := bh.log.WithField("remote_peer", conn.RemoteAddr())
-
-		l.Debug("connection accepted")
-		go bh.handle(conn.WithContext(log.Set(conn.Context(), l)))
-	}
-}
-
-func (bh basicHost) handle(conn *net.Conn) {
-	defer bh.Disconnect(conn.RemoteAddr())
-
-	var err error
-	var s *net.Stream
-	for range ctx.Tick(ctx.Link(bh.c, conn.Context())) {
-		if s, err = conn.AcceptStream(); err != nil {
-			bh.log.WithError(err).Warn("failed to accept stream")
-			return
-		}
-
-		l := bh.log.WithFields(log.F{
-			"remote_peer": s.RemoteAddr(),
-			"stream":      s.StreamID(),
-		})
-		l.Debug("handling stream")
-
-		c := log.Set(s.Context(), l)
-		go bh.handleStream(s.WithContext(c))
-	}
-}
-
-func (bh basicHost) handleStream(s *net.Stream) {
-	var p net.Path
-	if _, err := p.ReadFrom(s); err != nil {
-		bh.log.WithError(err).Error("failed to read stream path")
-		return
-	}
-
-	l := bh.log.WithFields(log.F{"locus": "handler", "path": p})
-	c := log.Set(s.Context(), l)
-
-	bh.mux.Serve(p.String(), s.WithContext(c))
-}
-
-/*
-	implment StreamManager
-*/
-
-func (bh basicHost) Register(path string, h net.Handler) {
-	c := log.Set(bh.c, bh.log.WithLocus("mux"))
-	bh.mux.Register(c, path, h)
-}
-
-func (bh basicHost) Unregister(p net.Path) { bh.mux.Unregister(p.String()) }
-
-func (bh basicHost) Open(a casm.Addresser, p net.Path) (*net.Stream, error) {
-	conn, ok := bh.peers.Get(a.Addr())
-	if !ok {
-		return nil, errors.New("peer not connected")
-	}
-
-	s, err := conn.OpenStream()
-	if err != nil {
-		return nil, errors.Wrap(err, "open stream")
-	}
-
-	l := bh.log.WithField("stream", s.StreamID())
-	l.Debug("stream opened")
-
-	if _, err = p.WriteTo(s); err != nil {
-		return nil, errors.Wrap(err, "write path")
-	}
-
-	l = l.WithField("path", p)
-	l.Debug("header sent")
-
-	c := log.Set(s.Context(), l)
-	return s.WithContext(c), nil
-}
-
-/*
-	Implement Network
-*/
-
-func (bh basicHost) Connect(c context.Context, a casm.Addresser) error {
-
-	if _, connected := bh.peers.Get(a.Addr()); connected {
-		return errors.Errorf("%s already connected", a.Addr().ID())
-	}
-
-	conn, err := bh.t.Dial(c, a.Addr())
-	if err != nil {
-		return errors.Wrap(err, "transport")
-	}
-
-	if !bh.peers.Add(conn) {
-		conn.Close()
-		return errors.New("peer already connected")
-	}
-
-	bh.log.WithField("remote_peer", conn.RemoteAddr()).Debug("connected to peer")
-	return nil
-}
-
-func (bh basicHost) Disconnect(id casm.IDer) {
-	if conn, ok := bh.peers.Del(id.ID()); ok {
-		conn.Close()
-	}
 }
