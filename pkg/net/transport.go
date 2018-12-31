85a82e6c6e22454cd8a002cce501aa04f0968193
Louis Thibault <l.thibault@sentimens.com>
2018-12-31 15:24:23 +0100

    Remove additional exports from transport.go

--- pkg/net/transport.go
+++ pkg/net/transport.go
@@ -8,23 +8,6 @@ import (
 	"github.com/pkg/errors"
 )
 
-// ProtoDialer can initiate connection upgrades using the casm network protocol.
-type ProtoDialer interface {
-	Dial(context.Context, Addr) (*Conn, error)
-}
-
-// ProtoListener can produce a listener that negotiates connection upgrades
-// according to the casm network protocol.
-type ProtoListener interface {
-	Listen(context.Context) (*Listener, error)
-}
-
-// Upgrader negotiates a connection upgrade according to the CASM protocol
-type Upgrader interface {
-	UpgradeDialer(pipe.Conn, Addr, PeerID) error
-	UpgradeListener(pipe.Conn, Addr) (remote Addr, err error)
-}
-
 type (
 	pipeDialer interface {
 		Dial(context.Context, net.Addr) (pipe.Conn, error)
@@ -43,41 +26,38 @@ type (
 	}
 )
 
-// Transport is an abstraction over a reliable network connection.  The NewDialer
-// and NewListener methods bind a dialback/listen address, respectively, to the
-// ProtoDialer & ProtoListener.
+// Transport is an abstraction over a reliable network connection.
 type Transport struct{ pt pipe.Transport }
 
-// NewTransport binds a pipewerks Transport to an Upgrader.
+// NewTransport based on pipewerks.
 func NewTransport(t pipe.Transport) *Transport {
 	return &Transport{pt: t}
 }
 
 // NewDialer binds a dialback Addr to the Transport
 func (t Transport) NewDialer(dialback Addr) ProtoDialer {
-	return dialer{pipeDialer: t.pt, local: dialback, u: upgrader}
+	return ProtoDialer{pipeDialer: t.pt, local: dialback, u: upgrader}
 }
 
 // NewListener binds a listen Addr to the Transport
 func (t Transport) NewListener(listen Addr) ProtoListener {
-	return listener{pipeListener: t.pt, local: listen, u: upgrader}
+	return ProtoListener{pipeListener: t.pt, local: listen, u: upgrader}
 }
 
-type dialer struct {
+// ProtoDialer can initiate connection upgrades using the casm network protocol.
+type ProtoDialer struct {
 	local Addr
 	pipeDialer
 	u dialUpgradeHandler
 }
 
-func (d dialer) Dial(c context.Context, a Addr) (*Conn, error) {
+// Dial into a remote Listener
+func (d ProtoDialer) Dial(c context.Context, a Addr) (*Conn, error) {
 	pc, err := d.pipeDialer.Dial(c, a)
 	if err != nil {
 		return nil, errors.Wrap(err, "dial pipe")
 	}
 
-	// Handshake
-	// 1. announce who we are to the listener (who has no idea right now)
-	// 2. verify that PIDs match
 	if err = d.u.UpgradeDialer(pc, d.local, a.ID()); err != nil {
 		return nil, errors.Wrap(err, "upgrade")
 	}
@@ -85,13 +65,16 @@ func (d dialer) Dial(c context.Context, a Addr) (*Conn, error) {
 	return &Conn{Conn: pc, local: d.local, remote: a}, nil
 }
 
-type listener struct {
+// ProtoListener can produce a ProtoListener that negotiates connection upgrades
+// according to the casm network protocol.
+type ProtoListener struct {
 	local Addr
 	pipeListener
 	u listenUpgradeHandler
 }
 
-func (l listener) Listen(c context.Context) (*Listener, error) {
+// Listen for incoming connections
+func (l ProtoListener) Listen(c context.Context) (*Listener, error) {
 	pl, err := l.pipeListener.Listen(c, l.local)
 	if err != nil {
 		return nil, errors.Wrap(err, "listen pipe")
